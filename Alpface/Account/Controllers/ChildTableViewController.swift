09bc81937743f7ff56e401acf55377ba4609e2b7
swae <xiaoyuan1314@me.com>
2018-04-06 02:45:45 +0800

    Update user profile.

---
+++ Alpface/Account/Controllers/ChildTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ChildTableViewController.swift
+//  UserProfileExample
+//
+//  Created by swae on 2018/4/5.
+//  Copyright © 2018年 alpface. All rights reserved.
+//
+
+import UIKit
+
+@objc(ALPChildTableViewController)
+class ChildTableViewController: UITableViewController, ProfileViewChildControllerProtocol {
+    func childScrollView() -> UIScrollView? {
+        return self.tableView
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "reuseIdentifier")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+ 
+}
--- Alpface/Account/Controllers/UserProfileViewController.swift
+++ Alpface/Account/Controllers/UserProfileViewController.swift
@@ -13,23 +13,8 @@ let ALPNavigationBarHeight: CGFloat = 44.0
 let ALPStatusBarHeight: CGFloat = UIApplication.shared.statusBarFrame.height
 
 @objc(ALPProfileViewController)
-class UserProfileViewController: ProfileViewController {
+class UserProfileViewController: BaseProfileViewController {
     
-    fileprivate lazy var videosTableView: UITableView = {
-        let tableView = UITableView(frame: CGRect.zero, style: .plain)
-        return tableView
-    }()
-    fileprivate lazy var favoritesTableView: UITableView = {
-        let tableView = UITableView(frame: CGRect.zero, style: .plain)
-        return tableView
-    }()
-    fileprivate lazy var storysTableView: UITableView = {
-        let tableView = UITableView(frame: CGRect.zero, style: .plain)
-        return tableView
-    }()
-    
-    var custom: UIView!
-    var label: UILabel!
 
     override func numberOfSegments() -> Int {
         return 3
@@ -39,38 +24,15 @@ class UserProfileViewController: ProfileViewController {
         return "Segment \(index)"
     }
     
-    override func prepareForLayout() {
-        
-        self.automaticallyAdjustsScrollViewInsets = false
-        if #available(iOS 11.0, *) {
-            videosTableView.contentInsetAdjustmentBehavior = .never
-            favoritesTableView.contentInsetAdjustmentBehavior = .never
-            storysTableView.contentInsetAdjustmentBehavior = .never
-        } else {
-            // Fallback on earlier versions
-        }
-        
-        videosTableView.delegate = self
-        videosTableView.dataSource = self
-        videosTableView.register(UITableViewCell.self, forCellReuseIdentifier: "tweetCell")
-        
-        favoritesTableView.delegate = self
-        favoritesTableView.dataSource = self
-        favoritesTableView.register(UITableViewCell.self, forCellReuseIdentifier: "photoCell")
-        
-        storysTableView.delegate = self
-        storysTableView.dataSource = self
-        storysTableView.register(UITableViewCell.self, forCellReuseIdentifier: "favCell")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.locationString = "Beijing"
-        self.username = AuthenticationManager.shared.loginUser?.username
-        if let userid = AuthenticationManager.shared.loginUser?.userid {
-            self.nickname = "用户号" + ":\(userid)"
-        }
+        self.nickname = "alpface"
+        self.username = "xiaoyuan"//AuthenticationManager.shared.loginUser?.username
+//        if let userid = AuthenticationManager.shared.loginUser?.userid {
+//            self.nickname = "用户号" + ":\(userid)"
+//        }
         
         self.profileImage = UIImage.init(named: "icon.png")
     }
@@ -85,59 +47,19 @@ class UserProfileViewController: ProfileViewController {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
     
-    override func scrollView(forSegment index: Int) -> UIScrollView {
+    override func controller(forSegment index: Int) -> ProfileViewChildControllerProtocol {
         switch index {
         case 0:
-            return videosTableView
+            return ChildTableViewController()
         case 1:
-            return favoritesTableView
+            return ChildTableViewController()
         case 2:
-            return storysTableView
+            return ChildTableViewController()
         default:
-            return videosTableView
+            return ChildTableViewController()
         }
     }
 }
 
 
-
-// MARK: UITableViewDelegates & DataSources
-extension UserProfileViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch tableView {
-        case videosTableView:
-            return 30
-        case favoritesTableView:
-            return 10
-        case storysTableView:
-            return 0
-        default:
-            return 10
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch tableView {
-        case videosTableView:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath)
-            cell.textLabel?.text = "Row \(indexPath.row)"
-            return cell
-            
-        case favoritesTableView:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath)
-            cell.textLabel?.text = "Photo \(indexPath.row)"
-            return cell
-            
-        case storysTableView:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "favCell", for: indexPath)
-            cell.textLabel?.text = "Fav \(indexPath.row)"
-            return cell
-            
-        default:
-            return UITableViewCell()
-        }
-    }
-
-}
 
